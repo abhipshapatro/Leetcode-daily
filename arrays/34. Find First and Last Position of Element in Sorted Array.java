ba404c877e2d1b11e8be78d997431c8824bebb0d
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2021-10-30 21:32:07 +0530

    find the starting and ending position of a given target value

---
+++ arrays/34. Find First and Last Position of Element in Sorted Array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target){
+                for (int j = nums.length-1 ; j >= i ; j--) {
+                    if (nums[j] == target){
+                        return new int[] {i,j};
+                    }
+                }
+            }
+        }
+        return new int[] {-1,-1};
+    }
+}
