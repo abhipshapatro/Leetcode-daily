5302ed00ee785da153c0746cdf5b5d7d0484f029
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2022-09-25 19:49:46 +0530

    plus One

--- arrays/66. Plus One.java
+++ arrays/66. Plus One.java
@@ -1,16 +1,21 @@
-class Solution{
-    public int[] plusOne(int[] digits){
-        int lastPos = digits.length - 1;
-        for(int i = lastPos; i>0 ; i--){
+class Solution {
+    public int[] plusOne(int[] digits) {
+        if(digits == null){
+            throw new IllegalArgumentException("null array");
+        }
+        int lastPos = digits.length;
+        if( lastPos == 0){
+            return new int[] {1};
+        }
+        for(int i = lastPos - 1; i>=0 ; i--){
             if(digits[i] < 9){
                 digits[i]++;
                 return digits;
             }
             digits[i] = 0;
-
         }
 
-        //incase of {9,9,9}
+        //incase of {9,9}
         int newArr[] = new int[digits.length + 1];
         newArr[0] = 1;
         return newArr;
