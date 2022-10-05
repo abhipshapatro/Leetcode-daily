92fc0932c5ce69853dd1ec9a0ca22ee09bfb4f98
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2022-10-05 14:59:52 +0530

    Sort Colors

--- arrays/75. Sort Colors.java
+++ arrays/75. Sort Colors.java
@@ -2,16 +2,24 @@ class Solution {
     public void sortColors(int[] nums) {
         int i = 0;
         int j = nums.length -1 ;
+        int nz = 0;
         while( i < j ){
-            if(nums[i] <= nums[j]){
+            if(nums[i]==0){
+                int temp=nums[i];
+                nums[i]=nums[nz];
+                nums[nz]=temp;
+                nz++;
                 i++;
             }
-            else{
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
+            else if(nums[i]==2){
+                int temp1=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp1;
                 j--;
-            }   
+            }
+            else{
+                i++;
+            }
         }
     }
 }
