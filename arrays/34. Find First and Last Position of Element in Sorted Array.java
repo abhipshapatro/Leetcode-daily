07567e67209772d17821f2e96d9793186281d695
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2022-10-08 23:03:48 +0530

    TC : log(n)

--- arrays/34. Find First and Last Position of Element in Sorted Array.java
+++ arrays/34. Find First and Last Position of Element in Sorted Array.java
@@ -1,16 +1,41 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         
-        
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target){
-                for (int j = nums.length-1 ; j >= i ; j--) {
-                    if (nums[j] == target){
-                        return new int[] {i,j};
-                    }
-                }
-            }
+        int[] arr = new int[2];
+        arr[0] = binaryFirstSearch(nums, target);
+        arr[1] = binaryLastSearch(nums, target);
+        return arr;
+    }
+    public int binaryFirstSearch(int[] nums, int target){
+        int l = 0;
+        int r = nums.length - 1;
+        int index = -1;
+        while(l <= r){
+            int mid = (l + r) / 2;
+            
+            if(nums[mid] == target)
+                index = mid;
+            if(nums[mid] >= target)
+                r = mid - 1;
+            else l = mid + 1;
+                
         }
-        return new int[] {-1,-1};
+        return index;
+    }
+    public int binaryLastSearch(int[] nums, int target){
+        int l = 0;
+        int r = nums.length - 1;
+        int index = -1;
+        while(l <= r){
+            int mid = (l + r) / 2;
+            
+            if(nums[mid] <= target)
+                l = mid + 1;
+            else r = mid - 1;
+            if(nums[mid] == target)
+                index = mid;
+            
+        }
+        return index;
     }
 }
