25bddbd0445ab019b0ea5bc8b6d519122a2cccd2
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2022-10-10 21:15:39 +0530

    Number of Arithmetic Triplets

---
+++ arrays/2367. Number of Arithmetic Triplets.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int arithmeticTriplets(int[] nums, int diff) {
+        HashSet<Integer> set = new HashSet <>();
+        int count = 0;
+        
+        for(int i = 0; i < nums.length ; i++)
+            set.add(nums[i]);
+        
+        for(int i = 0; i < nums.length ; i++){
+            if(set.contains(nums[i] + diff) && set.contains(nums[i] - diff))
+                count++;
+        }
+        return count;
+    }
+}
