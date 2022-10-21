6090404c62b5012691f86d5f18c300bcd22c1c66
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2022-10-22 00:33:47 +0530

    wave array

---
+++ arrays/Wave Array.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Solution {
+    public int[] wave(int[] A) {
+        Arrays.sort(A);
+        for(int i = 0; i < A.length - 1; i+=2){
+            int temp = A[i];
+            A[i] = A[i+1];
+            A[i+1] = temp;
+        }
+        return A;
+    }
+}
+
+//arraylist
+// public class Solution {
+//     public ArrayList<Integer> wave(ArrayList<Integer> A) {
+//         //corner case
+//         if(A.size() == 1)
+//         return A;
+//         Collections.sort(A);
+//         for(int i = 0 ; i < A.size() - 1 ; i+=2){
+//             Collections.swap(A, i, i+1);
+//         }
+//         return A;
+//     }
+// }
