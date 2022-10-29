fcc5925b39875ad8e2baad063661aa3c40836493
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2022-10-29 20:05:29 +0530

    leaders in an array

---
+++ 0-INTERVIEW BIT/Leaders in an array.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public ArrayList<Integer> solve(ArrayList<Integer> A) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        int max = Integer.MIN_VALUE;
+        for(int i = A.size()-1 ; i>=0; i--){
+            if(A.get(i) > max){
+                ans.add(A.get(i));
+                max = A.get(i);
+            }
+        }
+        return ans;
+    }
+}
