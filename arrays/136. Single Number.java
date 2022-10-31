ac7c9b5eba5bcdfb47786bb69182e9d8927a5d1a
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2022-10-31 23:27:19 +0530

    single Number

---
+++ arrays/136. Single Number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        int singleNum = 0;
+        for(int i = 0; i < nums.length; i++){
+            singleNum ^= nums[i];          
+        } 
+        return singleNum;
+       
+    }
+}
